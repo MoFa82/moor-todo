4dcce15798324aa6bebb2bd4bc2d40f0d937cc07
Mojtaba <mojtabafazeliyan@gmail.com>
2024-10-05 03:17:14 +0330

    Add the class Task args and printing the welcome text

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # a cli todo app with ant theme
 
-class Row:
-    def __init__(self):
-        pass
+class Task:
+    def __init__(self, desc, checked):
+        self.desc = desc
+        self.checked = checked
 
 def main_menu():
+    print("Help Moori the ant to do his tasks")
     commad = input()
     while command != 0 or command != "exit":
         pass
