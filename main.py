18ff4cdfec7011a375dccae7e6a151da459a3128
Mojtaba <mojtabafazeliyan@gmail.com>
2024-10-29 23:11:26 +0330

    Adding the ant ascii art, fixing minor syntax errors including missed self, notting self.marked in task class and defining the dict variable needed for converting to json

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # a cli todo app with ant theme
-#TODO: consider making a menu class might be useful for future use 
+#TODO: consider making a menu class might be useful for future use
 #TODO: Get the ascii art of an ant
 #TODO: saving the info into json matters
 #TODO: adding the ability to set timer
@@ -22,7 +22,7 @@ class Group:
     def add_task(self, task):
         self.tasks.append(task)
         return "Task added succesfully"
-    
+
     def get_title(self):
         return self.title
 
@@ -30,28 +30,30 @@ class Group:
         return self.tasks
 
     def print_group(self):
-        print("======== " + self.get_title() + " ========")    
+        print("======== " + self.get_title() + " ========")
         for i in self.get_tasks():
             print(str(i.get_marked()) + " " + i.get_desc())
 
     def to_dict(self):
-        group_dict["title"] = self.get_title()
-        group_dict["tasks"] = [] 
+        self.group_dict["title"] = self.get_title()
+        self.group_dict["tasks"] = []
+        task_dict = {}
         for i in self.get_tasks():
             task_dict[i.get_desc()] = i.get_marked()
-            group_dict["tasks"].append(task_dict)
-            group_dict.clear()
+            self.group_dict["tasks"].append(task_dict)
+            task_dict.clear()
+            self.group_dict.clear()
 
-        return group_dict
+        return self.group_dict
 
 class Task:
     def __init__(self, desc, marked=False):
         self.set_desc(desc)
         self.marked = marked
-    
+
     def mark(self):
-        self.marked != self.marked
-           
+        self.marked = not self.marked
+
     def set_desc(self, desc):
         self.desc = desc
 
@@ -70,7 +72,7 @@ def save_to_disk(groups):
     with open("moor.json", "w") as json_file:
         for i in groups:
             json.dumps(i, json_file)
-    f.close()
+    json_file.close()
 
 def update_file():
     with open("moor.json", "a") as f:
@@ -83,7 +85,7 @@ def new_group():
 def print_all_groups(groups):
     print("========All groups=========")
     for i in range(len(groups)):
-        print(i)    
+        print(i)
         groups[i].print_group()
         print("+++++++++++++\n")
 
@@ -103,7 +105,39 @@ def add_task_to_group(group):
 def main_menu():
     groups = []
     command = ""
+    ANT_ART = """
+
+              '=.
+                           '=. \\
+                              \\ \\
+                           _,-=\\/=._        _.-,_
+                          /         \\      /=-._ '-.
+                         |=-./~\\___/~\\    /     `-._\
+                         |   \\o/   \\o/   /         /
+                          \\_   `~~~;/    |         |
+                            `~,._,-'    /          /
+                               | |      =-._      /
+                           _,-=/ \\=-._     /|`-._/
+                         //           \\   )\
+                        /|             |)_.'/
+                       //|             |\\_.''   _.-\\
+                      (|  \\           /    _.`=    \\
+                      ||   ":_    _.;"_.-;"   _.-=.:
+                   _-."/    / `-."\\_."        =-_.;\
+                  `-_./   /             _.-=.    / \\
+                         |              =-_.;\\ .'   \\
+                         \\                   \\/     \\
+                         /\\_                .'\\      \\
+                        //  `=_         _.-"   \\      \\
+                       //      `~-.=`"`'       ||      ||
+                       ||    _.-_/|            ||      |\\_.-_
+                   _.-_/|   /_.-._/            |\\_.-_  \\_.-._\\
+                  /_.-._/                      \\_.-._\\
+
+
+    """
     while command != "0" or command != "exit":
+        print(ANT_ART)
         print("hello, I'm Moori!\nI'm here to help you doing your tasks\nWhat can I do for you?\n")
         print("0- Exit\n1- Add new group\n2- Show all groups and tasks\n3- Toggle between groups")
 
@@ -124,15 +158,15 @@ def main_menu():
 
         elif command == "2":
             print_all_groups(groups)
-        
+
         elif command == "3":
             print_all_groups(groups)
             print("Enter which group you want to toggle to:")
             inp = int(input())
 
-            group = groups[inp] 
-            print("You chose %s\nWhat you want to do with it?", group.get_title()) 
-            
+            group = groups[inp]
+            print("You chose %s\nWhat you want to do with it?", group.get_title())
 
-if __name__ == "__main__" : 
+
+if __name__ == "__main__" :
     main_menu()
